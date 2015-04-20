60767c46e448514cc9c9e812a44ee1af107accc6
Cathy Ji <catherhj@usc.edu>
2015-04-19 23:59:01 -0700

    github pages won't execute php oops...

--- contact.php
+++
@@ -1,8 +0,0 @@
-<div id="contact" class="section">
-	<hr/>
-	<h2>get in contact</h2>
-	<p><a href="https://www.linkedin.com/in/catherineji" target="_blank">LinkedIn</a></p>
-	<p><a href="https://github.com/cather" target="_blank">Github</a></p>
-	<p><a href="mailto:jicathyh@gmail.com">Email</a></p>
-	<hr/>
-</div> <!-- contact -->
